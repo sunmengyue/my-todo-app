c804311c0e2967c1762a487cdb391ca4de86f894
Simon <bardrabbit@gmail.com>
2020-12-15 20:07:06 -0500

    add a modal dialog before deleting

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Todo from './Todo';
 import '../styles/TodoList.css';
+import Modal from './UI/Modal';
 
 const TodoList = ({ deleteTodo, toggleTodo, editTodo, filteredTodos }) => {
+  const [show, setShow] = useState(false);
+  const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false);
+
   const todoItems = filteredTodos.map((todo) => {
     return (
       <Todo
@@ -16,18 +21,37 @@ const TodoList = ({ deleteTodo, toggleTodo, editTodo, filteredTodos }) => {
     );
   });
   return (
-    <>
+    <div>
+      <Modal show={show}>
+        <p>
+          <i className='material-icons md-48 danger'>priority_high</i>Are you
+          sure you want to delete all tasks
+        </p>
+        <div className='float-end'>
+          <button type='button' className='cancleButton' onClick={handleClose}>
+            No
+          </button>
+          <button type='button' className='confirmButton'>
+            Yes
+          </button>
+        </div>
+      </Modal>
+
       <div className='list-group mt-5'>{todoItems}</div>
       {window.location.pathname === '/completed' ? (
         <div className='d-flex'>
-          <button type='button' className='btn btn-danger mt-5 ms-auto'>
+          <button
+            type='button'
+            className='btn btn-danger mt-5 ms-auto'
+            onClick={handleShow}
+          >
             Delete All
           </button>
         </div>
       ) : (
         ''
       )}
-    </>
+    </div>
   );
 };
 
---
+++ src/components/UI/Modal.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Modal.css';
+
+const Modal = ({ children, show }) => {
+  return (
+    <div
+      className='confirmBox'
+      style={{
+        transform: show ? 'translateY(0)' : 'translateY(-100vh)',
+        opacity: show ? '1' : '0',
+      }}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Modal;
